74367dbb90b7934783dce8341dc1e573f23421d2
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-07-22 16:33:14 -0400

    commit tree 49fe324589d1be61494b321cc4d4c8178a53ebe9

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
-  
+    belongs_to :game #same as belongs_to(:game), we are calling method without parentheses because it looks nicer
+
+    # a review belongs to a game 
+    # def game 
+        # self is the review instance
+        # Game.find(self.game_id)
+    # end
 end
